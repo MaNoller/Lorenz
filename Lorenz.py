65adc5f1a587b53abede7f395fcf2feab35860c8
Marcel Noller <96265697+MaNoller@users.noreply.github.com>
2022-03-16 13:40:29 +0100

    Update Lorenz.py

--- Lorenz.py
+++ Lorenz.py
@@ -1,6 +1,7 @@
 from scipy.integrate import solve_ivp
 import numpy as np
 t_m=100
+timepts=10000
 x=np.zeros(t_m)
 y=np.zeros(t_m)
 z=np.zeros(t_m)
@@ -15,12 +16,8 @@ def Lorenz_cont(t,Z,sigma,rho,beta):
     dz=x*y-beta*z
     return dx,dy,dz
 
-def Lorenz_discr(x,y,z,n,sigma,rho,beta):
-    for i in n:
-        x[i+1]=x[i]+schrittweite*(sigma*(y[i]-x[i]))
-        y[i+1]=y[i]+schrittweite*(x[i]*(rho-z[i])-y[i])
-        z[i+1]=z[i]+schrittweite*(x[i]*y[i]-beta*z[i])
-    return x,y,z
 
-print('info', sigma,rho,beta)
 L_sol=solve_ivp(Lorenz_cont,(0,t_m),(x[0], y[0], z[0]),args=(sigma,rho,beta),dense_output=True, method='DOP853')
+
+t = np.linspace(0, t_m, timepts)
+x,y,z = L_sol.sol(t)
